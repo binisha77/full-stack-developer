898fa764aa8e40c07589ed9fae24e6bce5e7a9c7
sovitha <khadkabinisha48@gmail.com>
2024-08-16 19:29:31 +0545

    learning databse

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+require('dotenv').config()
 const express = require('express')
+const connectToDatabase = require('./database')
+
 const app = express()
+app.use(express.json())
+
+connectToDatabase()
+
 app.get("/",(req,res)=>{
-  res.json({
+  res.status(200).json({
     message : "this is home page"
   //  res.send("hello world")
 })
 })
-app.get("/about",(req,res)=>{
-  res.json({
-    message:"this is about page"
+// app.get("/about",(req,res)=>{
+//   res.json({
+//     message:"this is about page"
+//   })
+// })
+app.post("/blog",(req,res)=>{
+  console.log(req.body)
+  res.status(200).json({
+      message : "Blog api hit successfully"
   })
 })
-app.listen(3000,()=>{
+
+app.listen(process.env.PORT,()=>{
   console.log("nodejs project has started")
 })
+// mongodb+srv://khadkabinisha48:@cluster0.dxm6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
